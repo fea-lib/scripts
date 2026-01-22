da542d98fcce188a88fe0f3757d8beaba16de0de
Tobias Belch <tobias.belch@docmorris.de>
2026-01-22 08:44:29 +0100

    fix: prevent multiple runs of post checkout hook

--- git-worktree/add.sh
+++ git-worktree/add.sh
@@ -22,12 +22,5 @@ git worktree add --detach "$WORKTREE_FOLDER" "$BRANCH_NAME"
 echo "✅ Worktree created. Navigating to directory..."
 cd "$WORKTREE_FOLDER" || exit
 
-echo "🚀 Attaching worktree to branch '$BRANCH_NAME'..."
-git checkout "$BRANCH_NAME"
-
-echo "🔗 Manually triggering post-checkout hook..."
-# The hooks are in the .bare directory for this setup
-"$GIT_ROOT/.bare/hooks/post-checkout" 0 0 1
-
 # Optional: If you use the symlink hook created previously, 
 # it will trigger automatically upon checkout.
